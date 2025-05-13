390d72d3ef2864a3145bc08306dc2700daeb5215
brkss <berkassebrahim@gmail.com>
2025-05-13 21:42:21 +0100

    multistage dockerfile build

--- internal/core/utils/password.go
+++ internal/core/utils/password.go
@@ -3,9 +3,6 @@ package utils
 import "golang.org/x/crypto/bcrypt"
 
 
-
-
-
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
